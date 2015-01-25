8e3b9f1b05a5a8811b4f6451e60a88ab5a11808a
famo <famo@ebay.com>
2015-01-25 23:05:07 +0800

    add user dao, TODO: MeetingService. not finished.
    need to re-define interface. @Jason we need to have a discussion with @Wil tomorrow afternoon.

--- src/main/scala/onionring/service/MeetingService.scala
+++ src/main/scala/onionring/service/MeetingService.scala
@@ -2,20 +2,50 @@ package onionring.service
 
 import akka.actor.{Props, Actor}
 import akka.pattern.ask
-import onionring.dao.{Get, MeetingDao}
-import onionring.pojo.Listing
+import onionring.dao.{GetUser, UserDao, Get, MeetingDao}
+import onionring.pojo.{User, Listing}
+import onionring.vo._
+import scala.concurrent.duration._
+
+import scala.concurrent.Await
 
 /**
 * Created by famo on 1/25/15.
 */
 class MeetingService extends Actor{
   lazy val meetingDao = context.system.actorOf(Props(new MeetingDao))
+  lazy val userDao = context.system.actorOf(Props(new UserDao))
 
   override def receive = {
-    case GetMeetingList(cityId: Long, offset:Long, limit: Long) => {
+    case _ =>
+//    case GetMeetingList(cityId: Long, offset:Long, limit: Long) => {
       // TODO query by city id
 //      val meeting = meetingDao ? Get(offset,limit)
-    }
+//      meeting.onSuccess{
+//        case result : List[Listing] =>
+//          sender ! result.map(meeting => {
+//            val userFuture = userDao ? GetUser(meeting.userId)
+//            val user = Await.result(userFuture, 100 days).asInstanceOf[User]
+//            MeetingListResponse(payload =
+//              val paging = Paging(offset,limit)
+//              val meetingMeetingListResponseMeetingList(
+//              MeetingSummary(meeting.listingId,
+//              meeting.subject,
+//              meeting.targetUser,
+//              meeting.money,
+//              meeting.createTime
+//              ),UserSummary(
+//                user.userId,
+//                user.name,
+//                user.jobTitle,
+//                user.photo,
+//                user.score
+//                )
+//              )
+//            )
+//          })
+//      }
+//    }
   }
 }
 
